90426b725f5df93ff2fa2949711c53bdcb767bc3
sashabull66 <76172037+sashabull66@users.noreply.github.com>
2023-12-18 12:34:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <div className="App-logo">*SASHA*</div>
         <p>
-          This is test React App Page
+         React App Page
         </p>
         <a
           className="App-link"
